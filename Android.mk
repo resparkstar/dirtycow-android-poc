d70de3aaa6dc28f37e14604c356f1262f4fa05e4
resparkstar <39150614+resparkstar@users.noreply.github.com>
2026-05-08 15:21:40 +0800

    Refactor Android.mk by removing dirtycow module
    Removed dirtycow module configuration and source files.

--- Android.mk
+++ Android.mk
@@ -1,18 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-  dirtycow.c \
-  dcow.c \
-
-LOCAL_MODULE := dirtycow
-LOCAL_LDFLAGS   += -llog
-LOCAL_CFLAGS    += -DDEBUG
-
-include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := run-as
 LOCAL_SRC_FILES := \
 dirtycow.c \
